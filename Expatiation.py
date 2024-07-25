af284111f1c55f97eec45ce1acecfc6eaae1ca35
codedreammer <nkanal38@gmail.com>
2024-07-25 18:15:51 +0530

    add the code

--- Expatiation.py
+++ Expatiation.py
@@ -1,8 +1,10 @@
 a = input("enter the number: ")
 print(f"Multiplication table of {a} is:")
 
-for i in range(1, 11):
-    print(f"{int(a)}X {i} = {int(a)*i}")
-
+try:
+    for i in range(1, 11):
+     print(f"{int(a)}X {i} = {int(a)*i}")
+except Exception as e:
+   print("sorry some error occured")
 print("some imp lines of code")
 print("end of program")
