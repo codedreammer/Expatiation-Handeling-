4d421303ed18d7cefa09aad8fa8f78e8d07738b5
codedreammer <nkanal38@gmail.com>
2024-07-25 18:22:58 +0530

    add some example

--- Expatiation.py
+++ Expatiation.py
@@ -1,10 +1,21 @@
-a = input("enter the number: ")
+'''a = input("enter the number: ")
 print(f"Multiplication table of {a} is:")
 
 try:
     for i in range(1, 11):
-     print(f"{int(a)}X {i} = {int(a)*i}")
+     print(f"{int(a)} X {i} = {int(a)*i}")
 except Exception as e:
-   print("sorry some error occured")
+   print("Invaild Input!")
 print("some imp lines of code")
 print("end of program")
+'''
+try:
+    num =int(input("enter an integer:"))
+    a = [6,3]
+    print(a[num])
+except ValueError:
+    print("number entered is not an integer")
+
+except IndexError:
+    print("Index Error")
+    
