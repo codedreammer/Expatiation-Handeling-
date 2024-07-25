cc5a6ad129ec196bf857550d04e14e342355f1d7
codedreammer <nkanal38@gmail.com>
2024-07-25 18:05:22 +0530

    add the file

---
+++ Expatiation.py
@@ -0,0 +1,8 @@
+a = input("enter the number: ")
+print(f"Multiplication table of {a} is:")
+
+for i in range(1, 11):
+    print(f"{int(a)}X {i} = {int(a)*i}")
+
+print("some imp lines of code")
+print("end of program")
