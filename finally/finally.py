f8d2617ef0396f77fec5dd974421b20bb4e8e485
codedreammer <nkanal38@gmail.com>
2024-07-25 19:01:16 +0530

    add the example

--- finally/finally.py
+++ finally/finally.py
@@ -1,10 +1,18 @@
-try:
-    l = [1,3,5,6,7,8]
-    i = int(input("enter the index: "))
-    print(l[i])
-    
-except:
+def fun1():
+ try:
+     l = [1,3,5,6,7,8]
+     i = int(input("enter the index: "))
+     print(l[i])
+     return 1
+ except:
     print("some error occured")
-    
-finally:
+    return 0
+ finally:
     print("I am always excucuted")
+ 
+x = fun1()
+print(x)
+
+
+    
+
